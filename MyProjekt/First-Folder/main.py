0cd12e33252451d029afb16c66808ff4d9cac3b6
RuslanIlyasov <rus.ilyasov.2017@inbox.ru>
2023-11-07 13:26:30 +0300

    add file

---
+++ MyProjekt/First-Folder/main.py
@@ -0,0 +1,15 @@
+first_word = "Some"  # String
+first_number_int = 7  # Integer
+first_number_flt = 5.68132  # Float
+first_var_bool = False  # Boolean
+
+result_word = "Результат:"
+
+first_number_int -= 5
+second_number_int = 8
+result_number = first_number_int % second_number_int
+
+a = z = q = 10
+w, r, b = 12, "Word", True
+
+print(result_word, result_number)
